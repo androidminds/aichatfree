b90589d7e3f5bffd788cc80e1bd74f45b275f587
richard@androidminds.cn <Richard@androiminds.cn>
2023-08-29 10:15:36 +0800

    adjust chat plugin

--- gpt3.5/deepai.py
+++ gpt3.5/deepai.py
@@ -4,38 +4,51 @@
 # Thanks to the original project and its developers.
 # 
 import requests
-import random
-import hashlib
+import js2py
 import json
-from fake_useragent import UserAgent
-
-def md5(text):
-    return hashlib.md5(text.encode()).hexdigest()[::-1]
-
-def get_api_key(user_agent):
-    part1 = str(random.randint(0, 10**11))
-    part2 = md5(user_agent + md5(user_agent + md5(user_agent+part1+"x")))
-    return f"tryit-{part1}-{part2}"
 
 def completion(messages, proxy=None):
-    url = "https://api.deepai.org/chat_response"
+    url = 'https://api.deepai.org/make_me_a_pizza'
 
-    user_agent = UserAgent().random
+    token_js = """
+        var agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'
+        var a, b, c, d, e, h, f, l, g, k, m, n, r, x, C, E, N, F, T, O, P, w, D, G, Q, R, W, I, aa, fa, na, oa, ha, ba, X, ia, ja, ka, J, la, K, L, ca, S, U, M, ma, B, da, V, Y;
+        h = Math.round(1E11 * Math.random()) + "";
+        f = function () {
+            for (var p = [], q = 0; 64 > q;) p[q] = 0 | 4294967296 * Math.sin(++q % Math.PI);
+            
+            return function (t) {
+                var v, y, H, ea = [v = 1732584193, y = 4023233417, ~v, ~y],
+                    Z = [],
+                    A = unescape(encodeURI(t)) + "\u0080",
+                    z = A.length;
+                t = --z / 4 + 2 | 15;
+                for (Z[--t] = 8 * z; ~z;) Z[z >> 2] |= A.charCodeAt(z) << 8 * z--;
+                for (q = A = 0; q < t; q += 16) {
+                    for (z = ea; 64 > A; z = [H = z[3], v + ((H = z[0] + [v & y | ~v & H, H & v | ~H & y, v ^ y ^ H, y ^ (v | ~H)][z = A >> 4] + p[A] + ~~Z[q | [A, 5 * A + 1, 3 * A + 5, 7 * A][z] & 15]) << (z = [7, 12, 17, 22, 5, 9, 14, 20, 4, 11, 16, 23, 6, 10, 15, 21][4 * z + A++ % 4]) | H >>> -z), v, y]) v = z[1] | 0, y = z[2];
+                    for (A = 4; A;) ea[--A] += z[A]
+                }
+                for (t = ""; 32 > A;) t += (ea[A >> 3] >> 4 * (1 ^ A++) & 15).toString(16);
+                return t.split("").reverse().join("")
+            }
+        }();
+
+        "tryit-" + h + "-" + f(agent + f(agent + f(agent + h + "x")));
+        """
+
+    data = {"chas_style": "chat", "chatHistory": messages}
+    api_key = js2py.eval_js(token_js)
     headers = {
-        "user-agent": user_agent,
-        "api-key": get_api_key(user_agent)
+        "api-key": api_key,
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
     }
 
-    files = {
-        "chat_style": (None, "chat"),
-        "chatHistory": (None, messages)
-    }
-
+    
     timeout = 10
     proxies = {'http': proxy, 'https': proxy} if proxy else None       
 
     try:
-        return requests.post(url, headers=headers, files=files, timeout=timeout, proxies=proxies, stream=True), True
+        return requests.post(url, headers=headers, data=data, timeout=timeout, proxies=proxies, stream=True), True
     except Exception as e:
         print("DeepAI post error: ", e)
         return None, False
